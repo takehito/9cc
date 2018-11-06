31602e39a7920f7983e782dc294126882ea36593
Fukuda Kento <utisama.produce@gmail.com>
2018-11-06 00:32:57 +0000

    数値を持つNode型のコンストラクタの追加

--- 9cc.c
+++ 9cc.c
@@ -127,3 +127,10 @@ Node *new_node(int op, Node *lhs, Node *rhs) {
 	node->rhs = rhs;
 	return node;
 }
+
+Node *new_node_num(int val) {
+	Node *node = malloc(sizeof(Node));
+	node->ty = ND_NUM;
+	node->val = val; 
+	return node;
+}
