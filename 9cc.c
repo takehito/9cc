faafe060cdd9f7b8e8acd64ac05ae6571a41c7e4
Fukuda Kento <utisama.produce@gmail.com>
2018-11-20 01:39:02 +0000

    変数と=を含んだ式をコンパイルできるように変更した

--- 9cc.c
+++ 9cc.c
@@ -8,6 +8,7 @@ void error(char *msg, char *result);
 // トークンの方を表す値
 enum {
 	TK_NUM = 256, // 整数トークン
+	TK_IDENT,	// 識別子トークン
 	TK_EOF, // 入力の終わりを表すトークン
 };
 
@@ -32,7 +33,7 @@ void tokenize(char *p) {
 			continue;
 		}
 
-		if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')') {
+		if (*p == '=' || *p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')' || *p == ';') {
 			tokens[i].ty = *p;
 			tokens[i].input = p;
 			i++;
@@ -45,6 +46,14 @@ void tokenize(char *p) {
 			tokens[i].input = p;
 			tokens[i].val = strtol(p, &p, 10);
 			i++;
+			continue;
+		}
+
+		if ('a' <= *p && *p <= 'z') {
+			tokens[i].ty = TK_IDENT;
+			tokens[i].input = p;
+			i++;
+			p++;
 			continue;
 		}
 
@@ -64,6 +73,7 @@ void error(char *msg, char *result) {
 
 enum {
 	ND_NUM = 256,	// 整数のノードの型
+	ND_IDENT,	// 変数のノードの型
 };
 
 typedef struct Node {
@@ -71,6 +81,7 @@ typedef struct Node {
 	struct Node *lhs;	// 左辺
 	struct Node *rhs;	// 右辺
 	int val;		// tyがND_NUMの場合のみ使う
+	char name;		// tyがND_IDENTの場合のみ使う
 } Node;
 
 Node *new_node(int op, Node *lhs, Node *rhs) {
@@ -88,14 +99,34 @@ Node *new_node_num(int val) {
 	return node;
 }
 
+Node *new_node_ident(char name) {
+	Node *node = malloc(sizeof(Node));
+	node->ty = ND_IDENT;
+	node->name = name;
+	return node;
+}
+
 Node *mul();
 Node *term();
+Node *expr();
 
 int pos = 0;
 
+Node *assign() {
+	Node *lhs = expr();
+	if (tokens[pos].ty == TK_EOF)
+		return lhs;
+	if (tokens[pos].ty == '=') {
+		pos++;
+		return new_node('=', lhs, assign());
+	}
+
+	return assign();
+}
+
 Node *expr() {
 	Node *lhs = mul();
-	if (tokens[pos].ty == TK_EOF || tokens[pos].ty == ')')
+	if (tokens[pos].ty == TK_EOF || tokens[pos].ty == ')' || tokens[pos].ty == '=')
 		return lhs;
 	if (tokens[pos].ty == '+') {
 		pos++;
@@ -110,7 +141,7 @@ Node *expr() {
 
 Node *mul() {
 	Node *lhs = term();
-	if (tokens[pos].ty == TK_EOF || tokens[pos].ty == '+' || tokens[pos].ty == '-' || tokens[pos].ty == ')')
+	if (tokens[pos].ty == TK_EOF || tokens[pos].ty == '+' || tokens[pos].ty == '-' || tokens[pos].ty == ')' || tokens[pos].ty == '=')
 		return lhs;
 	if (tokens[pos].ty == '*') {
 		pos++;
@@ -126,6 +157,8 @@ Node *mul() {
 Node *term() {
 	if (tokens[pos].ty == TK_NUM)
 		return new_node_num(tokens[pos++].val);
+	if (tokens[pos].ty == TK_IDENT)
+		return new_node_ident(*tokens[pos++].input);
 	if (tokens[pos].ty == '(') {
 		pos++;
 		Node *node = expr();
@@ -138,10 +171,39 @@ Node *term() {
 	error("数値でも開き括弧でもない出ないトークンです: %s\n", tokens[pos].input);
 }
 
+void gen_lval(Node *node) {
+	if (node->ty == ND_IDENT) {
+		printf("	mov rax, rbp\n");
+		printf("	sub rax, %d\n", ('z' - node->name + 1) * 8);
+		printf("	push rax\n");
+		return;
+	}
+	fprintf(stderr, "代入の左辺値が変数ではありません");
+}
+
 void gen(Node *node) {
 	// 式の最初は数でなければならないので、それをチェックして
 	if (node->ty == ND_NUM) {
 		printf("	push %d\n", node->val);
+		return;
+	}
+
+	if (node->ty == ND_IDENT) {
+		gen_lval(node);
+		printf("	pop rax\n");
+		printf("	move rax, [rax]\n");
+		printf("	push rax\n");
+		return;
+	}
+
+	if (node->ty == '=') {
+		gen_lval(node->lhs);
+		gen(node->rhs);
+
+		printf("	pop rdi\n");
+		printf("	pop rax\n");
+		printf("	mov [rax], rdi\n");
+		printf("	push rdi\n");
 		return;
 	}
 
@@ -180,12 +242,18 @@ int main(int argc, char **argv) {
 
 	// トークナイズしてパースする
 	tokenize(argv[1]);
-	Node* node = expr();
+	Node* node = assign();
 
 	// アセンブリの前半部分を出力
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
 	printf("main:\n");
+
+	// プロローグ
+	// 変数２６個分の領域を確保する
+	printf("	push rbp\n");
+	printf("	mov rbp, rsp\n");
+	printf("	sub rsp, 208\n");
 
 	// 抽象構文木を下りながらコード生成
 	gen(node);
@@ -193,6 +261,11 @@ int main(int argc, char **argv) {
 	// スタックトップに式全体の値が残っているはずなので
 	// それをRAXにロードして関数からの返り値とする
 	printf("	pop rax\n");
+
+	// エピローグ
+	// 最後の式の結果がRAXに残っているのでそれが返り血になる
+	printf("	mov rsp, rbp\n");
+	printf("	pop rbp\n");
 	printf("	ret\n");
 	return 0;
 }
